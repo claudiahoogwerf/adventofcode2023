63f04e6253fe19b68cbb5c7dd219cb6a5f81521c
Claudia Hoogwerf <claudiahoogwerf@claudias-mbp2.home>
2023-12-02 23:00:13 +0100

    made it work

--- week1/2/src/main.rs
+++ week1/2/src/main.rs
@@ -3,10 +3,11 @@ use std::io::{BufReader,BufRead};
 use std::error::Error;
 use std::path::Path;
 use std::collections::HashMap;
+use itertools::Itertools;
 
 fn main() {
     // Create a path to the desired file
-    let path = Path::new("input.txt");
+    let path = Path::new("../input.txt");
     let display = path.display();
 
     // Open the path in read-only mode, returns `io::Result<File>`
@@ -50,10 +51,41 @@ fn word_to_digits(string_org: String)-> String{
 }
 
 fn get_digit(string_org: String) -> i32{
-    let string = word_to_digits(string_org);
-    let digits_only: String = string.chars().filter(|c| c.is_digit(10)).collect();
+    let cleansed_string = clean_string(string_org);
+    let digit_string = word_to_digits(cleansed_string);
+    let digits_only: String = digit_string.chars().filter(|c| c.is_digit(10)).collect();
     let first_digit = digits_only.chars().nth(0).unwrap();
     let last_digit =  digits_only.chars().last().unwrap();
     let result = format!("{}{}", first_digit, last_digit);
     result.parse::<i32>().unwrap()
+}
+
+fn clean_string(string_org: String)-> String{
+    /*
+    This functions cleans the string so that for example oneight becomes oneeight
+    Also automatically strips all other characters that we don't need
+    */
+    let possible_digits = vec![
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
+    ];
+
+    // Collect all positions with the found substring in a hashmap
+    let mut positions: HashMap<i32, String> = HashMap::new();
+    for digit in possible_digits {
+        let positions_for_digit: Vec<_> = string_org.match_indices(digit).collect();
+        for p in positions_for_digit {
+            positions.insert(p.0 as i32, p.1.to_string());
+        }
+    }
+
+    // Sort that hashmap on position and print in a string again
+    let mut string: String = "".to_string();
+    for key in positions.keys().sorted() {
+        string = format!("{}{}", string, positions[key]);
+        println!("{:?} has {:?}", key, positions[key]);
+    }
+
+
+    string
 }
